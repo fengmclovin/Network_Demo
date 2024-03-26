1c43157f426501c3d420c58adfce9956301176e8
Lance Tian <79405882+fengmclovin@users.noreply.github.com>
2024-03-26 23:53:34 +1100

    Create sudoku.py

---
+++ Test_Code/sudoku.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        if not board:
+            return
+
+        self.solve(board)
+
+    def solve(self, board):
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    for num in map(str, range(1, 10)):
+                        if self.is_valid(board, i, j, num):
+                            board[i][j] = num
+                            if self.solve(board):
+                                return True
+                            board[i][j] = '.'
+                    return False
+        return True
+
+    def is_valid(self, board, row, col, num):
+        for i in range(9):
+            if board[row][i] == num or board[i][col] == num:
+                return False
+
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(start_row, start_row + 3):
+            for j in range(start_col, start_col + 3):
+                if board[i][j] == num:
+                    return False
+
+        return True
+
+# Example usage
+solution = Solution()
+board = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+solution.solveSudoku(board)
+for row in board:
+    print(row)
